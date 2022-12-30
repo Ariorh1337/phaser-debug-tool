0c14695d67d468017d244313845eed4a3bb2e431
Dmitry Omelchenko <omd1337@gmail.com>
2022-12-30 02:05:07 +0200

    fix inject issue

--- public/inject.js
+++ public/inject.js
@@ -6,8 +6,8 @@ try {
     url = chrome.runtime.getURL('index.js');
 }
 
-const tab = (browser || chrome).tabs.getCurrent();
-(browser || chrome).tabs.executeScript(tab.id, {
+const tab = (window.browser || window.chrome).tabs.getCurrent();
+(window.browser || window.chrome).tabs.executeScript(tab.id, {
     "file": url,
     "all_frames": true,
     "run_at": "document_start"
