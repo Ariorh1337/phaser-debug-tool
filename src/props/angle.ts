c7ddde82ac5edb428be1cc8e56b07595f04867bc
Dmitry Omelchenko <omd1337@gmail.com>
2023-01-28 16:45:55 +0200

    fix Group angle crash

--- src/props/angle.ts
+++ src/props/angle.ts
@@ -1,7 +1,7 @@
 import { hasProp } from "../utils/extra";
 
 export default function defineAngle(folder: any, obj: any) {
-    if (hasProp(obj, "angle")) {
+    if (hasProp(obj, "angle") && isFinite(obj.angle)) {
         const input = folder.addInput(obj, "angle", {
             step: 1,
         });
