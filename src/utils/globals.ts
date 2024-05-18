9f6bf2f4b58c78fbefecce8015b611993d136e7d
Dmitry Omelchenko <omd1337@gmail.com>
2024-05-18 22:27:27 +0200

    fix v3.11 uuid crash

--- src/utils/globals.ts
+++ src/utils/globals.ts
@@ -1,18 +1,32 @@
 export const gameObjList = {
     add: function (obj: any) {
-        const uuid = obj.DebugID || Phaser.Utils.String.UUID();
+        const UUID = (() => {
+            // Phaser v3.11 has no Phaser.Utils.String.UUID
+            if (Phaser.Utils.String.UUID) {
+                return Phaser.Utils.String.UUID();
+            } else if (Phaser.Math.RND.uuid) {
+                return Phaser.Math.RND.uuid();
+            } else {
+                console.error("🪲 Phaser UUID is not found");
+            }
+        });
+
+        const uuid = obj.DebugID || UUID();
 
         this.list[uuid] = obj;
         obj.DebugID = uuid;
 
         return uuid;
     },
+
     get: function (id: string) {
         return this.list[id];
     },
+
     remove: function (id: string) {
         delete this.list[id];
     },
+
     list: {} as { [key: string]: any },
 };
 
