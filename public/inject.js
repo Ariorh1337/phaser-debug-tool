cd6153c0abf907063340c9f36e4584328d33c617
Dmitry Omelchenko <omd1337@gmail.com>
2025-11-03 13:31:52 +0100

    better code injection

--- public/inject.js
+++ public/inject.js
@@ -13,9 +13,24 @@
         interceptorUrl = chrome.runtime.getURL('interceptor.js');
     }
 
-    document.documentElement.setAttribute('data-phaser-debug-url', indexUrl);
+	document.documentElement.setAttribute('data-phaser-debug-url', indexUrl);
+
+	try {
+		window.eval('');
+		const code = loadScript(interceptorUrl);
+		window.eval(code);
+		return;
+	} catch(err) {}
+
+	try {
+		(new Function(``))();
+		const code = loadScript(interceptorUrl);
+		new Function(code)();
+		return;
+	} catch(err) {}
 
     var script = document.createElement('script');
+	script.fetchPriority = 'high';
     script.setAttribute('type', 'text/javascript');
     script.setAttribute('src', interceptorUrl);
 	script.setAttribute('id', 'Phaser-Debugger');
@@ -23,3 +38,15 @@
     const target = document.head || document.documentElement;
     target.appendChild(script);
 })();
+
+function loadScript(url) {
+	const xhr = new XMLHttpRequest();
+	xhr.open('GET', url, false);
+	xhr.send(null);
+
+	if (xhr.status === 200) {
+		return xhr.responseText;
+	} else {
+		return '';
+	}
+}
